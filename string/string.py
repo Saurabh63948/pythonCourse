38434af09affaf283ac9d16931307098417e2da0
Saurabh63948 <saurabh123mahi@gmail.com>
2026-04-03 10:56:31 +0530

    string part is done

--- string/string.py
+++ string/string.py
@@ -1,1 +1,17 @@
 print("hello")
+chai_type="black" 
+customer_name="Priya"
+
+print(f"Oder for {customer_name}:{chai_type} please !")
+
+chai_des="nama"
+
+#slicing example
+
+print(f'First word: {chai_des[0:8:2]}')
+print(f"{chai_des[::-1]}")
+
+my_string = "नमस्ते saurabh"
+encoded_string = my_string.encode("utf-8")
+decoded_string = encoded_string.decode("utf-8")
+print(decoded_string)
