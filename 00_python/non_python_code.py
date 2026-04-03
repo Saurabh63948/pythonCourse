926d481197f3734f0244ca3a4553047b978c7448
Saurabh63948 <saurabh123mahi@gmail.com>
2026-04-03 09:39:41 +0530

    first commit of the lecture 13 data type

---
+++ 00_python/non_python_code.py
@@ -0,0 +1,5 @@
+is_boiling=True
+stri_count=5
+
+total_action=stri_count + is_boiling
+print(total_action)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pyhtonudemy!")
+
+
+if __name__ == "__main__":
+    main()
