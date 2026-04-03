6c5c991d8fa51658f42b02a1011705c838f9a9a5
Saurabh63948 <saurabh123mahi@gmail.com>
2026-04-03 10:27:36 +0530

    number sys completed now next is string

---
+++ 00_python/decimal.py
@@ -0,0 +1,10 @@
+import sys
+ideal_temp=95.5
+current_temp=95.499999999999999999999999999999999
+
+print(f"ideal temp {ideal_temp}")
+print(f"current temp {ideal_temp}")
+
+print(f"Difference temp {ideal_temp - current_temp }")
+print("hello world")
+print(sys.float_info)
