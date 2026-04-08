092c45689f6799c0b423c3c20d51f4a6d4d26f85
Saurabh63948 <saurabh123mahi@gmail.com>
2026-04-08 22:12:15 +0530

    "new changes"

---
+++ functions/01_duplicaton.py
@@ -0,0 +1,5 @@
+def print_order(name,chai_type):
+    print(f"{name} ordered {chai_type}")
+
+print_order("saurabh","masala chai")
+print_order("Aman","green chai")
---
+++ functions/02_spliting_complex_tasks.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+  print("Fetcing the sale data")
+
+def filter_valid_sale():
+  print("filtering the valid sales data") 
+
+def summarize_data():
+  print("Summarizing the sales data")
+
+def generate_report():
+  fetch_sales()
+  filter_valid_sale()
+  summarize_data()
+  print("Report is ready")  
+
+generate_report()  
